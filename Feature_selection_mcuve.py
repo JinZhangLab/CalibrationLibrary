dc33596ebe8c98a670951af91ba3c79b301c092d
chinn <24755028@qq.com>
2019-04-30 13:34:41 +0800

    Add MC-UVE

--- Feature_selection_mcuve.py
+++ Feature_selection_mcuve.py
@@ -1,4 +1,9 @@
-#
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019.4.30
+# @Author  : FrankEl
+# @File    : Feature_selection_demo_mcuve.py
+
 import scipy.io as scio
 import numpy as np
 from sklearn.cross_decomposition import PLSRegression
@@ -6,29 +11,57 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from calib.pls import MCuve
 
-
 if __name__ == "__main__":
     MatPath = './data/corn.mat'
     CornData = scio.loadmat(MatPath)
-    wv = np.linspace(1100, 2498, (2498-1100)//2+1, endpoint=True)
+    wv = np.linspace(1100, 2498, (2498 - 1100) // 2 + 1, endpoint=True)
     X = CornData["X"]
     Y = CornData["Y"]
-    print("Importted the dataset")
-
+    # Building normal pls model
     Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, Y[:, 0], test_size=0.2)
-    plsmodel = PLSRegression(n_components=7)
-    plsmodel.fit(Xtrain, Ytrain)
-    T, P, U, Q, W, C, beta = plsmodel.x_scores_, plsmodel.x_loadings_, plsmodel.y_scores_, plsmodel.y_loadings_, plsmodel.x_weights_, plsmodel.y_weights_, plsmodel.coef_
+    plsModel = PLSRegression(n_components=7)
+    plsModel.fit(Xtrain, Ytrain)
+    T, P, U, Q, W, C, beta = plsModel.x_scores_, plsModel.x_loadings_, plsModel.y_scores_, plsModel.y_loadings_, plsModel.x_weights_, plsModel.y_weights_, plsModel.coef_
     plt.plot(wv, beta[0:])
     plt.xlabel("Wavelength")
     plt.ylabel("Intensity")
     plt.title("Regression Coefficients")
     plt.show()
-
-    mcmodel = MCuve(Xtrain, Ytrain, 7)
-    stability = mcmodel.calcStability()
-    plt.plot(stability)
+    # Prediction result of pls model
+    Ytrain_hat = plsModel.predict(Xtrain)
+    Ytest_hat = plsModel.predict(Xtest)
+    plt.plot([Ytrain.min(), Ytrain.max()], [Ytrain.min(), Ytrain.max()], 'k--', lw=4)
+    plt.scatter(Ytrain, Ytrain_hat, marker='*')
+    plt.scatter(Ytest, Ytest_hat, marker='*')
+    plt.xlabel("Reference")
+    plt.ylabel("Prediction")
+    plt.title("Prediction of normal pls model")
+    plt.show()
+    # Stability of MC-UVE
+    mcModel = MCuve(Xtrain, Ytrain, 7)
+    mcModel.calcStability()
+    plt.plot(mcModel.stability)
     plt.xlabel("Wavelength")
     plt.ylabel("Intensity")
     plt.title("Stability of MCUVE")
+    plt.show()
+    # Feature ranking efficienty by stability of MC-UVE
+    mcModel.evalStability(cv=5)
+    plt.plot(mcModel.featureR2)
+    plt.xlabel("Wavelength")
+    plt.ylabel("Intensity")
+    plt.title("R2")
+    plt.show()
+    # Prediction results after feature selection by MC-UVE
+    XtrainNew, XtestNew = mcModel.cutFeature(Xtrain, Xtest)
+    plsModelNew = PLSRegression(n_components=7)
+    plsModelNew.fit(XtrainNew, Ytrain)
+    YtrainNew_hat = plsModelNew.predict(XtrainNew)
+    YtestNew_hat = plsModelNew.predict(XtestNew)
+    plt.plot([Ytrain.min(), Ytrain.max()], [Ytrain.min(), Ytrain.max()], 'k--', lw=4)
+    plt.scatter(Ytrain, YtrainNew_hat, marker='*')
+    plt.scatter(Ytest, YtestNew_hat, marker='*')
+    plt.xlabel("Reference")
+    plt.ylabel("Prediction")
+    plt.title("Prediction after MC-UVE")
     plt.show()
--- calib/pls.py
+++ calib/pls.py
@@ -1,6 +1,8 @@
 import numpy as np
 from sklearn.cross_decomposition import PLSRegression
+from sklearn.linear_model import  LinearRegression
 from sklearn.model_selection import ShuffleSplit
+from sklearn.model_selection import cross_val_score
 
 class MCuve:
     def __init__(self, x, y, nComp = 1, nrep = 500, testSize = 0.2):
@@ -9,9 +11,12 @@ class MCuve:
         self.ncomp = nComp
         self.nrep = nrep
         self.testSize = testSize
+        self.stability = None
+        self.featureIndex = None
+        self.featureR2 = np.empty(self.x.shape[1])
+        self.selFeature = None
 
     def calcStability(self):
-        stability = np.zeros(self.x.shape[1])
         PLSCoef = np.zeros((self.nrep, self.x.shape[1]))
         ss = ShuffleSplit(n_splits = self.nrep, test_size = self.testSize)
         step = 0
@@ -24,9 +29,30 @@ class MCuve:
             step += 1
         meanCoef = np.mean(PLSCoef, axis=0)
         stdCoef = np.std(PLSCoef, axis=0)
-        stability = meanCoef/stdCoef
-        return stability
+        self.stability = meanCoef/stdCoef
 
+    def evalStability(self, cv=3, n_jobs=None):
+        self.featureIndex = np.argsort(-np.abs(self.stability))
+        for i in range(self.x.shape[1]):
+            if i<self.ncomp:
+                regModel = LinearRegression()
+            else:
+                regModel = PLSRegression(self.ncomp)
+            xi = self.x[:, self.featureIndex[:i+1]]
+            cvScore = cross_val_score(regModel, xi, self.y, cv=cv)
+            self.featureR2[i] = np.mean(cvScore)
+
+    def cutFeature(self, *args):
+        cuti = np.argmax(self.featureR2)
+        self.selFeature = self.featureIndex[:cuti+1]
+        if len(args) != 0:
+            returnx = list(args)
+            i = 0
+            for argi in args:
+                if argi.shape[1] == self.x.shape[1]:
+                    returnx[i] = argi[:, self.selFeature]
+                i += 1
+        return tuple(returnx)
 
 if __name__ == "__main__":
     print("This is the PLS model")
